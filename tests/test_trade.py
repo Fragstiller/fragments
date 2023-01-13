272ec8ffc9876aa24715b74a0818124ae45675b7
Fragstiller <fragstiller90@gmail.com>
2023-01-13 23:09:04 +0300

    stricter trade test

--- tests/test_trade.py
+++ tests/test_trade.py
@@ -1,11 +1,12 @@
 import unittest
 from fragments.strategy import *
-from fragments.params import ParamStorage
 
 
 class TestTrade(unittest.TestCase):
     def test_trade_long_pos(self):
         trade = Trade(TradeDirection.LONG, 100.0)
+        trade.forward((0, 0, 0, 1, 0))
+        trade.forward((0, 0, 0, 0.5, 0))
         trade.forward((0, 0, 0, 1, 0))
         trade.forward((0, 0, 0, 2, 0))
         self.assertEqual(trade.profit, 100.0)
@@ -13,11 +14,15 @@ class TestTrade(unittest.TestCase):
     def test_trade_long_neg(self):
         trade = Trade(TradeDirection.LONG, 100.0)
         trade.forward((0, 0, 0, 1, 0))
+        trade.forward((0, 0, 0, 2, 0))
+        trade.forward((0, 0, 0, 1, 0))
         trade.forward((0, 0, 0, 0.5, 0))
         self.assertEqual(trade.profit, -50.0)
 
     def test_trade_short_pos(self):
         trade = Trade(TradeDirection.SHORT, 100.0)
+        trade.forward((0, 0, 0, 1, 0))
+        trade.forward((0, 0, 0, 1.5, 0))
         trade.forward((0, 0, 0, 1, 0))
         trade.forward((0, 0, 0, 0.5, 0))
         self.assertEqual(trade.profit, 50.0)
@@ -25,11 +30,15 @@ class TestTrade(unittest.TestCase):
     def test_trade_short_neg(self):
         trade = Trade(TradeDirection.SHORT, 100.0)
         trade.forward((0, 0, 0, 1, 0))
+        trade.forward((0, 0, 0, 0.5, 0))
+        trade.forward((0, 0, 0, 1, 0))
         trade.forward((0, 0, 0, 2, 0))
         self.assertEqual(trade.profit, -100.0)
 
     def test_trade_long_liq(self):
         trade = Trade(TradeDirection.LONG, 100.0)
+        trade.forward((0, 0, 0, 1, 0))
+        trade.forward((0, 0, 0, 2, 0))
         trade.forward((0, 0, 0, 1, 0))
         trade.forward((0, 0, 0, 0, 0))
         self.assertEqual(trade.profit, -100.0)
@@ -39,6 +48,8 @@ class TestTrade(unittest.TestCase):
 
     def test_trade_short_liq(self):
         trade = Trade(TradeDirection.SHORT, 100.0)
+        trade.forward((0, 0, 0, 1, 0))
+        trade.forward((0, 0, 0, 0.5, 0))
         trade.forward((0, 0, 0, 1, 0))
         trade.forward((0, 0, 0, 2, 0))
         self.assertEqual(trade.profit, -100.0)
