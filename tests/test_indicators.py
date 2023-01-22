b1d007184df143759890cd4feb32b006345b1fff
Fragstiller <fragstiller90@gmail.com>
2023-01-22 12:17:10 +0300

    more tests

--- tests/test_indicators.py
+++ tests/test_indicators.py
@@ -5,6 +5,33 @@ from fragments.params import ParamStorage
 
 
 class TestIndicators(unittest.TestCase):
+    def test_precalc_toggling(self):
+        ohlcv_list = [(0, 0, 0, 1, 0), (0, 0, 0, 2, 0), (0, 0, 0, 1, 0)]
+
+        Indicator.enable_precalculation(ohlcv_list)
+        rsi = RSI(ParamStorage())
+        self.assertEqual(rsi.forward((0, 0, 0, 1, 0)), None)
+        self.assertEqual(rsi.forward((0, 0, 0, 2, 0)), None)
+        self.assertEqual(rsi.forward((0, 0, 0, 1, 0)), 50.0)
+        Indicator.disable_precalculation()
+
+        rsi.reset()
+        self.assertEqual(rsi.forward((0, 0, 0, 1, 0)), None)
+        self.assertEqual(rsi.forward((0, 0, 0, 2, 0)), None)
+        self.assertEqual(rsi.forward((0, 0, 0, 1, 0)), 50.0)
+
+        Indicator.enable_precalculation(ohlcv_list)
+        rsi.reset()
+        self.assertEqual(rsi.forward((0, 0, 0, 1, 0)), None)
+        self.assertEqual(rsi.forward((0, 0, 0, 2, 0)), None)
+        self.assertEqual(rsi.forward((0, 0, 0, 1, 0)), 50.0)
+        Indicator.disable_precalculation()
+
+        rsi.reset()
+        self.assertEqual(rsi.forward((0, 0, 0, 1, 0)), None)
+        self.assertEqual(rsi.forward((0, 0, 0, 2, 0)), None)
+        self.assertEqual(rsi.forward((0, 0, 0, 1, 0)), 50.0)
+
     def test_precalc_rsi(self):
         ohlcv_list = [(0, 0, 0, 1, 0), (0, 0, 0, 2, 0), (0, 0, 0, 1, 0)]
         Indicator.enable_precalculation(ohlcv_list)
@@ -26,6 +53,8 @@ class TestIndicators(unittest.TestCase):
 
         Indicator.enable_precalculation(ohlcv_list)
         atr = ATR(ParamStorage())
+        atr.period.value = 2
+        atr.reset()
         self.assertEqual(atr.forward(ohlcv_list[0]), None)
         self.assertEqual(atr.forward(ohlcv_list[1]), None)
         self.assertAlmostEqual(atr.forward(ohlcv_list[2]), 2.42, 2)  # type: ignore
@@ -37,12 +66,15 @@ class TestIndicators(unittest.TestCase):
             ohlcv_list = pkl.load(f)
 
         atr = ATR(ParamStorage())
+        atr.period.value = 2
+        atr.reset()
         self.assertEqual(atr.forward(ohlcv_list[0]), None)
         self.assertAlmostEqual(atr.forward(ohlcv_list[1]), 1.87, 2)  # type: ignore
         self.assertAlmostEqual(atr.forward(ohlcv_list[2]), 2.33, 2)  # type: ignore
 
     def test_precalc_sma(self):
         ohlcv_list = [(0, 0, 0, 1, 0), (0, 0, 0, 2, 0), (0, 0, 0, 1, 0)]
+        Indicator.enable_precalculation(ohlcv_list)
         sma = SMA(ParamStorage())
         self.assertEqual(sma.forward((0, 0, 0, 1, 0)), None)
         self.assertEqual(sma.forward((0, 0, 0, 2, 0)), 1.5)
--- tests/test_optim.py
+++ tests/test_optim.py
@@ -2,9 +2,9 @@ import unittest
 import pickle as pkl
 import os
 from fragments.params import ParamStorage
-from fragments.strategy import ConditionalStrategy
-from fragments.indicators import RSI
-from fragments.stats import total_profit
+from fragments.strategy import ConditionalStrategy, CrossoverStrategy, LimiterStrategy
+from fragments.indicators import RSI, SMA, ATR, Indicator
+from fragments.stats import equity
 from fragments.optim import *
 
 
@@ -14,7 +14,9 @@ class TestOptim(unittest.TestCase):
         param_storage = ParamStorage()
 
         strategies = list()
-        strategies.append(ConditionalStrategy(RSI(param_storage), param_storage))
+        strategies.append(
+            CrossoverStrategy(SMA(param_storage), SMA(param_storage), param_storage)
+        )
         strategies.append(
             ConditionalStrategy(RSI(param_storage), param_storage, strategies[0])
         )
@@ -23,7 +25,107 @@ class TestOptim(unittest.TestCase):
             ohlcv_list = pkl.load(f)
 
         self.assertAlmostEqual(
-            optimize(strategies[-1], total_profit, ohlcv_list, random_state=42).fun,
-            0.0250,
+            optimize(strategies[-1], equity, ohlcv_list, random_state=42).fun,
+            -152.4914,
             4,
         )
+
+    @unittest.skipIf(os.getenv("TEST_OPTIM") is None, "TEST_OPTIM is not set")
+    def test_consistency_after_optim_precalc(self):
+        param_storage = ParamStorage()
+
+        with open("./data/GOOG.pkl", "rb") as f:
+            ohlcv_list = pkl.load(f)
+
+        Indicator.enable_precalculation(ohlcv_list)
+
+        strategies = list()
+        strategies.append(
+            CrossoverStrategy(SMA(param_storage), SMA(param_storage), param_storage)
+        )
+        strategies.append(
+            ConditionalStrategy(RSI(param_storage), param_storage, strategies[-1])
+        )
+
+        results = optimize(strategies[-1], equity, ohlcv_list, random_state=42)
+        first_equity = strategies[-1].equity
+
+        Indicator.disable_precalculation()
+
+        strategies[-1].update_and_forward_all(results.x, ohlcv_list)
+        second_equity = strategies[-1].equity
+
+        self.assertEqual(first_equity, second_equity)
+
+    @unittest.skipIf(os.getenv("TEST_OPTIM") is None, "TEST_OPTIM is not set")
+    def test_layered_optim(self):
+        param_storage = ParamStorage()
+
+        with open("./data/GOOG.pkl", "rb") as f:
+            ohlcv_list = pkl.load(f)
+
+        strategies = list()
+        strategies.append(
+            CrossoverStrategy(SMA(param_storage), SMA(param_storage), param_storage)
+        )
+        strategies.append(
+            ConditionalStrategy(RSI(param_storage), param_storage, strategies[-1])
+        )
+
+        _ = optimize(strategies[-1], equity, ohlcv_list, random_state=42)
+        first_equity = strategies[-1].equity
+
+        param_storage = ParamStorage()
+
+        strategies.append(
+            LimiterStrategy(ATR(param_storage), param_storage, strategies[-1])
+        )
+
+        second_results = optimize(strategies[-1], equity, ohlcv_list, random_state=42)
+        self.assertAlmostEqual(
+            second_results.fun,
+            -105.4931,
+            4,
+        )
+        second_equity = strategies[-2].equity
+
+        self.assertEqual(first_equity, second_equity)
+
+    @unittest.skipIf(os.getenv("TEST_OPTIM") is None, "TEST_OPTIM is not set")
+    def test_consistency_of_precalc_and_nonprecalc_optim(self):
+        param_storage = ParamStorage()
+
+        with open("./data/GOOG.pkl", "rb") as f:
+            ohlcv_list = pkl.load(f)
+
+        Indicator.enable_precalculation(ohlcv_list)
+
+        strategies = list()
+        strategies.append(
+            CrossoverStrategy(SMA(param_storage), SMA(param_storage), param_storage)
+        )
+        strategies.append(
+            ConditionalStrategy(RSI(param_storage), param_storage, strategies[-1])
+        )
+
+        first_results = optimize(strategies[-1], equity, ohlcv_list, random_state=42)
+        first_equity = strategies[-1].equity
+
+        Indicator.disable_precalculation()
+
+        param_storage = ParamStorage()
+
+        strategies = list()
+        strategies.append(
+            CrossoverStrategy(SMA(param_storage), SMA(param_storage), param_storage)
+        )
+        strategies.append(
+            ConditionalStrategy(RSI(param_storage), param_storage, strategies[-1])
+        )
+
+        second_results = optimize(strategies[-1], equity, ohlcv_list, random_state=42)
+        second_equity = strategies[-1].equity
+
+        self.assertEqual(first_results.fun, second_results.fun)
+        self.assertEqual(first_results.x, second_results.x)
+        self.assertEqual(first_equity, second_equity)
